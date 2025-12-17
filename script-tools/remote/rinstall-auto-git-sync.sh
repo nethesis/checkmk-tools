c922b7851b37ed07f903db16f31e83fbe21d2f48
Marzio Bordin <marzio@nethesis.it>
2025-12-17 21:31:34 +0100

    Fix: formattazione rinstall-auto-git-sync.sh

--- script-tools/remote/rinstall-auto-git-sync.sh
+++ script-tools/remote/rinstall-auto-git-sync.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Launcher per install-auto-git-sync.sh (usa script locale aggiornato da auto-git-sync)
+
 LOCAL_SCRIPT="/opt/checkmk-tools/script-tools/full/install-auto-git-sync.sh"
-# Esegue lo script localeexec "$LOCAL_SCRIPT" "$@"
+
+# Esegue lo script locale
+exec "$LOCAL_SCRIPT" "$@"
