ea5fd7fb0ff6eb09f3299c1edf3b317e2d6ba3aa
Marzio Bordin <marzio@nethesis.it>
2026-02-27 16:04:07 +0100

    fix(nsec8): v1.1.0 check_martian_packets.sh - rimossa logica rp_filter

--- script-check-nsec8/full/check_martian_packets.sh
+++ script-check-nsec8/full/check_martian_packets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # CheckMK plugin - Monitoraggio Martian Packets
 # Controlla pacchetti con IP sorgente/destinazione non validi
+# Version: 1.1.0 - rimossa logica rp_filter (normale su firewall)
 
 # Configurazione soglie
 WARN_THRESHOLD=10
@@ -51,27 +52,6 @@ if [[ ${#all_ips[@]} -gt 0 ]]; then
     unique_ips=$(printf '%s\n' "${all_ips[@]}" | sort -u | wc -l)
 fi
 
-# Verifica configurazione rp_filter (Reverse Path Filtering)
-rp_filter_status="unknown"
-rp_filter_all=0
-rp_filter_default=0
-
-if [[ -f /proc/sys/net/ipv4/conf/all/rp_filter ]]; then
-    rp_filter_all=$(cat /proc/sys/net/ipv4/conf/all/rp_filter 2>/dev/null || echo 0)
-fi
-
-if [[ -f /proc/sys/net/ipv4/conf/default/rp_filter ]]; then
-    rp_filter_default=$(cat /proc/sys/net/ipv4/conf/default/rp_filter 2>/dev/null || echo 0)
-fi
-
-if [[ $rp_filter_all -eq 1 ]] || [[ $rp_filter_default -eq 1 ]]; then
-    rp_filter_status="strict"
-elif [[ $rp_filter_all -eq 2 ]] || [[ $rp_filter_default -eq 2 ]]; then
-    rp_filter_status="loose"
-else
-    rp_filter_status="disabled"
-fi
-
 # Determina stato
 status=0
 status_text="OK"
@@ -85,16 +65,13 @@ elif [[ $martian_count -ge $WARN_THRESHOLD ]]; then
 elif [[ $martian_count -gt 0 ]]; then
     status=0
     status_text="OK - $martian_count martian packets (sotto soglia)"
-elif [[ "$rp_filter_status" == "disabled" ]]; then
-    status=1
-    status_text="WARNING - rp_filter disabilitato (nessun martian rilevato)"
 else
     status=0
-    status_text="OK - Nessun martian packet, rp_filter: $rp_filter_status"
+    status_text="OK - Nessun martian packet"
 fi
 
 # Output CheckMK
-echo "$status Martian_Packets count=$martian_count;$WARN_THRESHOLD;$CRIT_THRESHOLD;0 unique_ips=$unique_ips - $status_text | martian_count=$martian_count unique_ips=$unique_ips rp_filter_all=$rp_filter_all rp_filter_default=$rp_filter_default"
+echo "$status Martian_Packets count=$martian_count;$WARN_THRESHOLD;$CRIT_THRESHOLD;0 unique_ips=$unique_ips - $status_text | martian_count=$martian_count unique_ips=$unique_ips"
 
 # Dettagli IP sorgente (se presenti)
 if [[ ${#martian_sources[@]} -gt 0 ]]; then
@@ -108,5 +85,4 @@ if [[ ${#martian_destinations[@]} -gt 0 ]]; then
     echo "0 Martian_Destinations - IPs: $dest_list"
 fi
 
-# Info configurazione rp_filter
-echo "0 RP_Filter_Status - Mode: $rp_filter_status (all=$rp_filter_all, default=$rp_filter_default)"
+
